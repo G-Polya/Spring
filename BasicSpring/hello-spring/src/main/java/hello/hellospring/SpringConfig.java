c8d66fb9ac3517d26b702f47e7a37e18f58e9ae4
G_Polya <wlskaka4@gmail.com>
2021-01-12 16:47:58 +0900

    jpa, Spring data jpa

--- BasicSpring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ BasicSpring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,32 +1,48 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-
-    public SpringConfig(DataSource dataSoruce){
-        this.dataSource = dataSoruce;
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
-    }
+//    private final DataSource dataSource;
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em, DataSource dataSource) {
+//        this.em = em;
+//        this.dataSource = dataSource;
+//    }
+//
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        //return new MemoryMemberRepository();
+//        //return new JdbcMemberRepository(dataSource);
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//
+//    }
 
 }
--- BasicSpring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ BasicSpring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -3,7 +3,7 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
